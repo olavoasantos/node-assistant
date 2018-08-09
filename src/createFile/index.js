9c4d159038ceeda50311cf146ea839559222a4f9
Olavo <olavo.a.santos@gmail.com>
2018-08-09 10:58:28 -0300

    added resursive creation of directory

--- src/createFile/index.js
+++ src/createFile/index.js
@@ -2,7 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const mustache = require('mustache');
 
+const findOrCreateDir = (dirPath) => {
+  try {
+    path.dirname(dirPath).split('/').slice(1).reduce((fullPath, dir) => {
+      const newPath = fullPath !== '' ? `${fullPath}/${dir}` : dir;
+      if (!fs.existsSync(newPath)) {
+        fs.mkdirSync(newPath);
+      }
+
+      return newPath;
+    }, '');
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
 const createFile = (filePath, template = '', variables = {}) => {
+  findOrCreateDir(filePath);
   const contents = mustache.render(template, variables);
 
   try {
