551fb1e78e4567e86c40dffa0d61d847c83dc01b
Olavo <olavo.santos@skipthedishes.ca>
2019-07-23 19:05:06 -0500

    REFACTOR: (PATHS) refactor module to main index file

--- packages/paths/src/index.ts
+++ packages/paths/src/index.ts
@@ -1,5 +1,17 @@
-import Module from './Module';
+import { IApp, IModule } from '@node-assistant/core';
+import Paths from './Paths';
 
-export { IPaths, tPathFetcher, tPathFetcherList, tPathsConfig } from './Paths/contracts';
+const Module: IModule = {
+  name: 'paths',
+  install(app: IApp) {
+    return new Paths(app.config.get('paths'));
+  },
+  toolbox(app: IApp) {
+    return {
+      paths: app.modules.paths.list,
+    };
+  },
+};
 
 export default Module;
+export { IPaths, tPathFetcher, tPathFetcherList, tPathsConfig } from './Paths/contracts';
