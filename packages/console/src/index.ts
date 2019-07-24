8a24b8879c074206a196b8cdb97ef822d012006f
Olavo <olavo.santos@skipthedishes.ca>
2019-07-23 19:04:48 -0500

    REFACTOR: (CONSOLE) refactor module to main index file

--- packages/console/src/index.ts
+++ packages/console/src/index.ts
@@ -1,3 +1,12 @@
-import Module from './Module';
+import { IModule } from '@node-assistant/core';
+import colorize from './colorize';
+import echo from './echo';
+
+const Module: IModule = {
+  name: 'console',
+  toolbox() {
+    return { colorize, echo };
+  },
+};
 
 export default Module;
